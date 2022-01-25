67edf3402f01edd1c13f16e0e5c62eb21bbebeef
Robert <robat.wright@gmail.com>
2022-01-25 18:26:44 +0100

    excluded conda env from remote rep

--- conda_envs/data/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/robi/research/conda_envs/data/lib"
-XML2_LIBS="-lxml2 -L/home/robi/research/conda_envs/data/lib -lz -L/home/robi/research/conda_envs/data/lib -llzma -lpthread -L/home/robi/research/conda_envs/data/lib -L/home/robi/research/conda_envs/data/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/home/robi/research/conda_envs/data/include/libxml2 -I/home/robi/research/conda_envs/data/include"
-MODULE_VERSION="xml2-2.9.12"
-
