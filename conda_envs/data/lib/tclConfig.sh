617dc108f99f57266abb35eccf9b0e40895f4f32
Robert <robat.wright@gmail.com>
2022-01-25 18:21:49 +0100

    updated conda env

--- conda_envs/data/lib/tclConfig.sh
+++ conda_envs/data/lib/tclConfig.sh
@@ -44,11 +44,11 @@ TCL_LIBS='-ldl -lz  -lpthread -lm'
 
 # Top-level directory in which Tcl's platform-independent files are
 # installed.
-TCL_PREFIX='/home/robi/Desktop/research/conda_envs/data'
+TCL_PREFIX='/home/robi/research/conda_envs/data'
 
 # Top-level directory in which Tcl's platform-specific files (e.g.
 # executables) are installed.
-TCL_EXEC_PREFIX='/home/robi/Desktop/research/conda_envs/data'
+TCL_EXEC_PREFIX='/home/robi/research/conda_envs/data'
 
 # Flags to pass to cc when compiling the components of a shared library:
 TCL_SHLIB_CFLAGS='-fPIC'
@@ -57,7 +57,7 @@ TCL_SHLIB_CFLAGS='-fPIC'
 TCL_CFLAGS_WARNING='-Wall -Wpointer-arith'
 
 # Extra flags to pass to cc:
-TCL_EXTRA_CFLAGS='-march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O2 -ffunction-sections -pipe -isystem /home/robi/Desktop/research/conda_envs/data/include -fdebug-prefix-map=/tmp/build/80754af9/tk_1632504419923/work=/usr/local/src/conda/tk-8.6.11 -fdebug-prefix-map=/home/robi/Desktop/research/conda_envs/data=/usr/local/src/conda-prefix -pipe -m64 -DNDEBUG -D_FORTIFY_SOURCE=2 -O2 -isystem /home/robi/Desktop/research/conda_envs/data/include'
+TCL_EXTRA_CFLAGS='-march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O2 -ffunction-sections -pipe -isystem /home/robi/research/conda_envs/data/include -fdebug-prefix-map=/tmp/build/80754af9/tk_1632504419923/work=/usr/local/src/conda/tk-8.6.11 -fdebug-prefix-map=/home/robi/research/conda_envs/data=/usr/local/src/conda-prefix -pipe -m64 -DNDEBUG -D_FORTIFY_SOURCE=2 -O2 -isystem /home/robi/research/conda_envs/data/include'
 
 # Base command to use for combining object files into a shared library:
 TCL_SHLIB_LD='${CC} ${CFLAGS} ${LDFLAGS} -shared'
@@ -79,7 +79,7 @@ TCL_DL_LIBS='-ldl'
 
 # Flags to pass to the compiler when linking object files into
 # an executable tclsh or tcltest binary.
-TCL_LD_FLAGS='-Wl,-O2 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,--disable-new-dtags -Wl,--gc-sections -Wl,-rpath,/home/robi/Desktop/research/conda_envs/data/lib -Wl,-rpath-link,/home/robi/Desktop/research/conda_envs/data/lib -L/home/robi/Desktop/research/conda_envs/data/lib -Wl,--export-dynamic '
+TCL_LD_FLAGS='-Wl,-O2 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,--disable-new-dtags -Wl,--gc-sections -Wl,-rpath,/home/robi/research/conda_envs/data/lib -Wl,-rpath-link,/home/robi/research/conda_envs/data/lib -L/home/robi/research/conda_envs/data/lib -Wl,--export-dynamic '
 
 # Flags to pass to cc/ld, such as "-R /usr/local/tcl/lib", that tell the
 # run-time dynamic linker where to look for shared libraries such as
@@ -104,11 +104,11 @@ TCL_BUILD_LIB_SPEC='-L/tmp/build/80754af9/tk_1632504419923/work/tcl8.6.11/unix -
 
 # String to pass to linker to pick up the Tcl library from its
 # installed directory.
-TCL_LIB_SPEC='-L/home/robi/Desktop/research/conda_envs/data/lib -ltcl8.6'
+TCL_LIB_SPEC='-L/home/robi/research/conda_envs/data/lib -ltcl8.6'
 
 # String to pass to the compiler so that an extension can
 # find installed Tcl headers.
-TCL_INCLUDE_SPEC='-I/home/robi/Desktop/research/conda_envs/data/include'
+TCL_INCLUDE_SPEC='-I/home/robi/research/conda_envs/data/include'
 
 # Indicates whether a version numbers should be used in -l switches
 # ("ok" means it's safe to use switches like -ltcl7.5;  "nodots" means
@@ -140,7 +140,7 @@ TCL_SRC_DIR='/tmp/build/80754af9/tk_1632504419923/work/tcl8.6.11'
 # List of standard directories in which to look for packages during
 # "package require" commands.  Contains the "prefix" directory plus also
 # the "exec_prefix" directory, if it is different.
-TCL_PACKAGE_PATH='{/home/robi/Desktop/research/conda_envs/data/lib} '
+TCL_PACKAGE_PATH='{/home/robi/research/conda_envs/data/lib} '
 
 # Tcl supports stub.
 TCL_SUPPORTS_STUBS=1
@@ -157,13 +157,13 @@ TCL_BUILD_STUB_LIB_SPEC='-L/tmp/build/80754af9/tk_1632504419923/work/tcl8.6.11/u
 
 # String to pass to linker to pick up the Tcl stub library from its
 # installed directory.
-TCL_STUB_LIB_SPEC='-L/home/robi/Desktop/research/conda_envs/data/lib -ltclstub8.6'
+TCL_STUB_LIB_SPEC='-L/home/robi/research/conda_envs/data/lib -ltclstub8.6'
 
 # Path to the Tcl stub library in the build directory.
 TCL_BUILD_STUB_LIB_PATH='/tmp/build/80754af9/tk_1632504419923/work/tcl8.6.11/unix/libtclstub8.6.a'
 
 # Path to the Tcl stub library in the install directory.
-TCL_STUB_LIB_PATH='/home/robi/Desktop/research/conda_envs/data/lib/libtclstub8.6.a'
+TCL_STUB_LIB_PATH='/home/robi/research/conda_envs/data/lib/libtclstub8.6.a'
 
 # Flag, 1: we built Tcl with threads enabled, 0 we didn't
 TCL_THREADS=1
--- conda_envs/data/lib/tkConfig.sh
+++ conda_envs/data/lib/tkConfig.sh
@@ -36,11 +36,11 @@ TK_LIBS='-lX11   -lpthread -ldl -lz  -lpthread -lm'
 
 # Top-level directory in which Tk's platform-independent files are
 # installed.
-TK_PREFIX='/home/robi/Desktop/research/conda_envs/data'
+TK_PREFIX='/home/robi/research/conda_envs/data'
 
 # Top-level directory in which Tk's platform-specific files (e.g.
 # executables) are installed.
-TK_EXEC_PREFIX='/home/robi/Desktop/research/conda_envs/data'
+TK_EXEC_PREFIX='/home/robi/research/conda_envs/data'
 
 # -I switch(es) to use to make all of the X11 include files accessible:
 TK_XINCLUDES=''
@@ -57,11 +57,11 @@ TK_BUILD_LIB_SPEC='-L/tmp/build/80754af9/tk_1632504419923/work/tk8.6.11/unix -lt
 
 # String to pass to linker to pick up the Tk library from its
 # installed directory.
-TK_LIB_SPEC='-L/home/robi/Desktop/research/conda_envs/data/lib -ltk8.6'
+TK_LIB_SPEC='-L/home/robi/research/conda_envs/data/lib -ltk8.6'
 
 # String to pass to the compiler so that an extension can
 # find installed Tk headers.
-TK_INCLUDE_SPEC='-I/home/robi/Desktop/research/conda_envs/data/include'
+TK_INCLUDE_SPEC='-I/home/robi/research/conda_envs/data/include'
 
 # Location of the top-level source directory from which Tk was built.
 # This is the directory that contains a README file as well as
@@ -88,10 +88,10 @@ TK_BUILD_STUB_LIB_SPEC='-L/tmp/build/80754af9/tk_1632504419923/work/tk8.6.11/uni
 
 # String to pass to linker to pick up the Tk stub library from its
 # installed directory.
-TK_STUB_LIB_SPEC='-L/home/robi/Desktop/research/conda_envs/data/lib -ltkstub8.6'
+TK_STUB_LIB_SPEC='-L/home/robi/research/conda_envs/data/lib -ltkstub8.6'
 
 # Path to the Tk stub library in the build directory.
 TK_BUILD_STUB_LIB_PATH='/tmp/build/80754af9/tk_1632504419923/work/tk8.6.11/unix/libtkstub8.6.a'
 
 # Path to the Tk stub library in the install directory.
-TK_STUB_LIB_PATH='/home/robi/Desktop/research/conda_envs/data/lib/libtkstub8.6.a'
+TK_STUB_LIB_PATH='/home/robi/research/conda_envs/data/lib/libtkstub8.6.a'
